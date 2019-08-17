ed61204c561b18f4194fc9dbdfb429b99cc2c379
jeremyd85 <jeremydevore85@gmail.com>
2019-08-16 23:57:23 -0500

    create a requirements.txt

--- cv_window.py
+++ cv_window.py
@@ -1,24 +1,37 @@
 import cv2
 import handlers
+from cv_image import Image
 
 
 class Window:
-    def __init__(self, name):
+    def __init__(self, name, video=False):
         self.name = name
         self.closed = False
+        self.video_capture = cv2.VideoCapture(0) if video else None
+        self.frame = None
         cv2.namedWindow(name, cv2.WINDOW_NORMAL)
 
     def update_image(self, image):
+        self.frame = image
         cv2.imshow(self.name, image.img)
 
     def close_after_key(self, key):
         if cv2.waitKeyEx(0) == key:
-            cv2.destroyAllWindows()
-            self.closed = True
+            self.close()
 
     def close(self):
+        if self.video_capture:
+            self.video_capture.release()
         cv2.destroyAllWindows()
+        self.closed = True
 
     def mouse_event(self, handler_fun, param=[]):
         cv2.setMouseCallback(self.name, handler_fun, param=param)
+
+    def get_frame(self):
+        if not self.video_capture:
+            return []
+        return_val, self.frame = self.video_capture.read()
+        return Image(cv_image=self.frame)
+
 
--- recognize.py
+++ recognize.py
@@ -1,1 +1,41 @@
 import face_recognition
+import os
+from cv_window import Window
+from cv_image import Image
+
+KNOWN_FACES = 'known_faces'
+
+
+class Recognizer:
+
+    def __init__(self, name, max_images=10):
+        self.name = name
+        self.known_path = os.path.join(KNOWN_FACES, name)
+        self.max_images = max_images
+        self.encodings = []
+
+    def _get_known_list(self):
+        known_list = os.listdir(self.known_path)
+        if len(known_list) > self.max_images:
+            known_list = known_list[:self.max_images]
+        return known_list
+
+    def get_encodings(self):
+        if not self.encodings:
+            known_list = self._get_known_list()
+            for image in known_list:
+                loaded_image = face_recognition.load_image_file(image)
+                self.encodings.append(face_recognition.face_encodings(loaded_image))
+        return self.encodings
+
+
+def main():
+    win = Window('Recognize', video=True)
+    win.close_after_key(27)
+    while not win.close:
+        win.update_image(win.get_frame())
+
+main()
+
+
+
